e91416540548b94c2b6a89eef9169b7473ef375b
ragrawal <rajaanova@gmail.com>
2020-06-13 08:19:04 +0530

    adding file for number of way change can be formed

---
+++ numberOfWaysForChange/MinNumberOfWaysForChange.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(NumberOfWayForChange([]int{1,5},6))
+}
+func NumberOfWayForChange(denom []int, k int ) int {
+	array := make([]int,k+1)
+	array[0] = 1
+	for _, deno := range denom {
+		for i := 1;i<len(array);i++{
+			if i < deno {
+				continue
+			}
+			array[i] = array[i] + array[i-deno]
+		}
+	}
+	return array[k]
+}
